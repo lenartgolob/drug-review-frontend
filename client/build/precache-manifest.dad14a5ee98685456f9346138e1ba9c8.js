e1d6dbc97211464418198fc6bf433999e30948f5
lenartgolob <lenart.golob17@gmail.com>
2020-04-14 17:26:54 +0200

    build fix

---
+++ client/build/precache-manifest.dad14a5ee98685456f9346138e1ba9c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e86f9a17e8e4f8ed4ed749666125fb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f5f7c8de5211bb0c5e5",
+    "url": "/static/css/main.aed9fb99.chunk.css"
+  },
+  {
+    "revision": "c00942d4f765e10febe2",
+    "url": "/static/js/2.f3502ea5.chunk.js"
+  },
+  {
+    "revision": "b8298e6e30c7d2f2dcc101a784046ad7",
+    "url": "/static/js/2.f3502ea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f5f7c8de5211bb0c5e5",
+    "url": "/static/js/main.af9e0c7b.chunk.js"
+  },
+  {
+    "revision": "bbfa1d06d84b6e5a4edf",
+    "url": "/static/js/runtime-main.98b877ac.js"
+  },
+  {
+    "revision": "9a76e74e107e6163dc82116b8236c132",
+    "url": "/static/media/login.9a76e74e.svg"
+  }
+]);
